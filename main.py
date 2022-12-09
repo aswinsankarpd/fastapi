7a82175ebef621e4ddc30e9b71bcf42d0cee28dd
Aswin Sankar P D <you@example.com>
2022-12-10 00:20:04 +0530

    routings

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ def index():
 	return {'data':{'name':'aswin'}}
 
 
-@app.get('/about')
-def about():
-	return "The about page"
+@app.get('/blog/unpublished')
+def unpublished():
+	return {'data':'These are unpublished blogs'}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id:int):
+	return 'Comments on blog with id: {}'.format(id)
+
+
+@app.get('/blog/{id}')
+def blog(id:int):
+	return {'data':id} 
+
+
+
